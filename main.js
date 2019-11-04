0dba515477bf9e82b3147f116e0b62cefcd1b280
kittyhollingsworth <kittyhollingsworth@gmail.com>
2019-11-04 18:39:01 -0500

    attempt at getting javascript and html file to work together

---
+++ main.js
@@ -0,0 +1,9 @@
+let board = new Map([[1,'X'],[2,'X'],[3,'X'],[4,''],[5,''],[6,''],[7,'O'],[8,'O'],[9,'O']]);
+
+console.log(board)
+
+//two methods sets and gets
+
+
+
+let test = document.getElementById('cell-0').innerHTML = 'X';
