49c61bf8119d06f78b789c630ddbdd381f2ad982
kittyhollingsworth <kittyhollingsworth@gmail.com>
2019-11-04 19:22:38 -0500

    can now set cells

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-let board = new Map([[1,'X'],[2,'X'],[3,'X'],[4,''],[5,''],[6,''],[7,'O'],[8,'O'],[9,'O']]);
+var board = new Map([[1,''],[2,''],[3,''],[4,''],[5,''],[6,''],[7,''],[8,''],[9,'']]);
 
-console.log(board)
 
 //two methods sets and gets
 
 
 
-let test = document.getElementById('cell-0').innerHTML = 'X';
+
+function buildBoard(){
+    document.getElementById('cell-0').innerHTML = board.get(1);
+    document.getElementById('cell-1').innerHTML = board.get(2);
+    document.getElementById('cell-2').innerHTML = board.get(3);
+    document.getElementById('cell-3').innerHTML = board.get(4);
+    document.getElementById('cell-4').innerHTML = board.get(5);
+    document.getElementById('cell-5').innerHTML = board.get(6);
+    document.getElementById('cell-6').innerHTML = board.get(7);
+    document.getElementById('cell-7').innerHTML = board.get(8);
+    document.getElementById('cell-8').innerHTML = board.get(9);
+}
+buildBoard();
+
+function setCell(key,value){
+    board.set(ket,value)
+    buildBoard();
+};
+
+board.addEventListener('ccell-0', () => { setCell(X) });
+
